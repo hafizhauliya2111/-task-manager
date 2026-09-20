68bcf5e7ea1f903a2437bc4c9cd919158f79f20d
muhammadhafizh134 <2500018134@webmail.uad.ac.id>
2026-09-20 16:04:43 +0700

    buat config dan helpers

--- backend/helpers/send_email.php
+++ backend/helpers/send_email.php
@@ -1,2 +1,29 @@
 <?php
-// TODO: fungsi kirim email via PHPMailer (dipakai untuk verifikasi, reset password)
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../config/smtp.php';
+
+function sendEmail($to, $subject, $body){
+    global $host, $port, $username, $password;
+    $mail = new PHPMailer(true);
+
+    try{
+        $mail-> isSMTP();
+        $mail-> Host = $host;
+        $mail-> SMTPAuth = true;
+        $mail-> Username = $username;
+        $mail-> Password = $password;
+        $mail-> SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail-> Port = $port;
+        $mail-> setFrom('noreply@taskmanager.com', 'Task Manager');
+        $mail-> addAddress($to);
+        $mail-> isHTML(true);
+        $mail-> Subject = $subject;
+        $mail-> Body = $body;
+        $mail-> send();
+    }catch (Exception $e){
+        echo "Mailer Error: " . $mail->ErrorInfo;
+        exit;
+    }
+}
